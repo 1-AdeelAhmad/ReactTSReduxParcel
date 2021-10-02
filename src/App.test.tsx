9fce5fb5af231d72eb7cd50b5c0253659ac70aa1
adeel <adeel3786@gmail.com>
2021-10-02 21:50:05 +0100

    updated test config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from "enzyme";
-import App from "../App";
+import App from "./App";
 
 describe('Test App Entry point', function () {
   it('should have a header tag with Hello world React!', function () {
